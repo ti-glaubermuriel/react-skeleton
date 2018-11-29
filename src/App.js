a5d02da64dadc97bf676ed4d3fe1054ddbd1ee62
Glauber Muriel <ti.glaubermuriel@gmail.com>
2018-11-29 19:06:47 -0200

    custom style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
-import { Layout, List, Menu, Button, Icon, Select, Card, Row, Col } from 'antd';
+import { Layout, Menu, Button, Icon, Select, Card, Row, Col, Avatar, Spin, Dropdown, Divider   } from 'antd';
+import Slider from "react-slick";
 import iconColor from './assets/icon_color.png';
 import './App.css';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
 
 const { SubMenu } = Menu;
 const { Header, Content, Footer, Sider } = Layout;
@@ -11,16 +15,57 @@ const Option = Select.Option;
 class App extends Component {
 
   render() {
+    const settings = {
+      dots: true, 
+      infinite: true,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      /*autoplay: true,
+      autoplaySpeed: 5000*/
+    };
+
+    const menuUser = (
+
+      <Menu  mode="inline" style={{width: 250}}>
+        <Menu.Item>
+        <div className="ant-list-item-meta">
+        <div className="ant-list-item-meta-avatar">
+        <span className="ant-avatar ant-avatar-circle ant-avatar-image" style={{marginTop: 5}}>
+        <Avatar>GV</Avatar>
+        </span>
+        </div>
+        <div className="ant-list-item-meta-content">
+        <h4 className="ant-list-item-meta-title" style={{marginBottom: -5}}>George Vitoriano</h4>
+        <div className="ant-list-item-meta-description"><small>Administrador</small></div>
+        </div>
+        </div>
+        </Menu.Item>
+        <Divider style={{margin: '5px 0'}} />
+         <Menu.Item>
+          <a rel="noopener noreferrer" href="" style={{color: 'red'}}>Encerrar sessão</a>
+        </Menu.Item>
+       
+      </Menu>
+    );
+
+    const antIcon = <Icon type="loading" style={{ fontSize: 38 }} spin />;
+
     return (
       <Layout style={{ minHeight: '100vh', padding: 0 }}>
 
         <Header style={{ position: 'fixed', zIndex: 1024, width: '100%' }}>
 
-          <Select defaultValue="lucy" style={{ width: 120 }} >
-            <Option value="jack">Jack</Option>
-            <Option value="lucy">Lucy</Option>
-            <Option value="disabled" disabled>Disabled</Option>
-            <Option value="Yiminghe">yiminghe</Option>
+         <Dropdown overlay={menuUser} trigger={['click']}>
+          <Button type="primary" shape="circle" icon="user" />
+         </Dropdown>
+
+        
+
+          <Select defaultValue="1"  className="select-drop" style={{ width: 200 }} >
+            <Option value="1">Hospital Santa Lúcia</Option>
+            <Option value="2">Hospital das Clínicas USP</Option>
+            <Option value="Yiminghe">Hospital Samaritano</Option>
           </Select>
 
         </Header>
@@ -55,13 +100,39 @@ class App extends Component {
             </Menu>
 
           </Sider>
-          <Layout style={{ padding: '70px 20px 20px 220px' }}>
+          <Layout style={{ padding: '80px 20px 20px 220px' }}>
+            <h1  className="title"> <Icon type="line-chart" /> Visão geral</h1>
+            <Content>
+                <Row >
+                <Col span={24} style={{padding: '15px 50px 50px'}}>
+                <Slider {...settings} >
+                  <div><div style={{padding: '0px 30px 10px 30px'}}><Card className="card-indicator"> <Spin size="large" /> </Card></div></div>
+                  <div><div style={{padding: '0px 30px 10px 30px'}}><Card><p>2</p></Card></div></div>
+                  <div><div style={{padding: '0px 30px 10px 30px'}}><Card><p>3</p></Card></div></div>
+                  <div><div style={{padding: '0px 30px 10px 30px'}}><Card><p>4</p></Card></div></div>
+                  <div><div style={{padding: '0px 30px 10px 30px'}}><Card><p>5</p></Card></div></div>
+                  <div><div style={{padding: '0px 30px 10px 30px'}}><Card><p>6</p></Card></div></div>
+                  <div><div style={{padding: '0px 30px 10px 30px'}}><Card><p>7</p></Card></div></div>
+                  <div><div style={{padding: '0px 30px 10px 30px'}}><Card><p>8</p></Card></div></div>
+                  <div><div style={{padding: '0px 30px 10px 30px'}}><Card><p>9</p></Card></div></div>
+                  </Slider>
+                  </Col>
+                </Row>
+                <Row gutter={16}>
+                  <Col span={8}>
+                    <Card title="Top Anestesistas" extra={<Button type="dashed" size="small" className="btn-details-all">Ver todos</Button>} loading={true} avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}>
+                    <p>Top Anestesias</p>
+                    </Card>
+                  </Col>
+                  <Col span={16}>
+                  <Card title="Top Fármacos" extra={<Button type="dashed" size="small" className="btn-details-all">Ver todos</Button>}loading={true} avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}>
+                    <p>Card content</p>
+                    </Card>
+                  </Col>
+                </Row>
+            </Content>
 
-            <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 280 }}>
-              Content
-        </Content>
-
-            <Footer style={{ textAlign: 'center' }}>
+            <Footer style={{ textAlign: 'center', paddingTop: 50  }}>
               Powered by <img src={iconColor} alt="Logo" />
             </Footer>
           </Layout>
